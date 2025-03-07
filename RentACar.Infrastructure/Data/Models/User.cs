0627c2a9740686f392bf460cad07b3d8772628b9
Димитър Банчев <114139864+Dimitar0304@users.noreply.github.com>
2025-03-07 15:45:00 +0200

    make user class

--- RentACar.Infrastructure/Data/Models/User.cs
+++ RentACar.Infrastructure/Data/Models/User.cs
@@ -4,5 +4,12 @@ namespace RentACar.Infrastructure.Data.Models
 {
     class User:IdentityUser
     {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public override string Email { get; set; } = null!;
+        public int Role { get; set; }
+        public string Phone { get; set; } = null!;
+        public DateTime DateRegistered { get; set; } = DateTime.UtcNow;
+
     }
 }
